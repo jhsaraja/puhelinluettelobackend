e484829d33aab53dc0964346de863a639f898ef5
jhsaraja <jaana.h.sarajarvi@gmail.com>
2022-01-18 14:58:46 +0200

    Exrecise 3.3

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ app.get('/info', (req, res) => {
   res.send('<p>Phonebook has info for ' + number + ' people</p><p>' + date +'</p>')
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id) 
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+
+  if (person) {    
+    response.json(person)  
+  } else {    
+    response.status(404).end()  
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
